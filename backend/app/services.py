19045f3a273b0b6a47b1df9e9fbff8da4d4dd3b2
Braeden Duval <braedenduval3@gmail.com>
2026-08-06 13:37:11 -0700

    feat: write_policy. checks if directory exists, creates if not, then writes to policy file.

--- backend/app/services.py
+++ backend/app/services.py
@@ -1,11 +1,23 @@
+import os   
+from git import Repo
+
 class GitStorageService:
     def __init__(self, repo_path: str = "../policy_data_store"):
         self.repo_path = repo_path
+        self.repo = Repo(self.repo_path)
 
     def write_policy(self, tenant_id: str, filename: str, content: str) -> str:
-        
+        tenant_folder_path = os.path.join(self.repo_path, tenant_id)
+        full_file_path = os.path.join(tenant_folder_path, filename)
 
-        pass
+        os.makedirs(tenant_folder_path, exist_ok=True)
+        with open(full_file_path, "w") as f:
+            f.write(content)
+
+        self.repo.index.add([full_file_path])
+        commit = self.repo.index.commit(f"Add/Update policy {filename} for tenant {tenant_id}")
+
+        return commit.hexsha
 
     def delete_policy(self, tenant_id: str, filename: str):
 
